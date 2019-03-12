fdde6e066a789936f0159bdf5e1e54101bae6f26
hcq0313 <853801007@qq.com>
2019-03-12 16:15:57 +0800

    打包合并

---
+++ dist/bundle.js
@@ -0,0 +1,34 @@
+$(function(){
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#arear');
+
+  $btnCal.click(function(){
+    var w = Number($width.val()),
+        h = Number($height.val());
+
+  var p = 2*(w+h),
+      a = w*h;
+
+  $perimeter.val(p);
+  $area.val(a);
+  });
+});
+;/* exported Rectangle */
+function Rectangle(width, height) {
+    var w = Number(width),
+              h = Number(height);
+
+    this.area = function() {
+          return w * h;
+            
+    };
+
+    this.perimeter = function() {
+          return 2 * (w + h);
+            
+    };
+
+}
